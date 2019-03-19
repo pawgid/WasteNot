eec3fc256df8d4e305490d4b2a5c6f914882d1b8
pawgid <pgidaszewski@gmail.com>
2019-03-19 19:53:59 +0100

    Added on click for items on recycler view

--- app/src/main/java/com/example/wastenot/adapter/CustomAdapter.java
+++ app/src/main/java/com/example/wastenot/adapter/CustomAdapter.java
@@ -2,11 +2,13 @@ package com.example.wastenot.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wastenot.R;
 import com.example.wastenot.model.RetroList;
@@ -14,6 +16,8 @@ import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import static android.content.ContentValues.TAG;
 
 public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomViewHolder> {
 
@@ -25,7 +29,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         this.dataList = dataList;
     }
 
-    class CustomViewHolder extends RecyclerView.ViewHolder {
+    class CustomViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         public final View mView;
 
@@ -35,9 +39,16 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.CustomView
         CustomViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
+            itemView.setOnClickListener(this);
 
             txtTitle = mView.findViewById(R.id.title);
             coverImage = mView.findViewById(R.id.coverImage);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Log.d(TAG, "onClick " + getPosition() + " " + mView);
+            Toast.makeText(context, "You clicked item number: " + getPosition(), Toast.LENGTH_SHORT).show();
         }
     }
 
