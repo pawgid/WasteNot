2077881a921b28e35f168098cea0aec738b58170
pawgid <pgidaszewski@gmail.com>
2019-03-23 09:16:44 +0100

    Added maps integration and on click to go to the maps activity

--- app/src/main/java/com/example/wastenot/activities/DescriptionActivity.java
+++ app/src/main/java/com/example/wastenot/activities/DescriptionActivity.java
@@ -1,9 +1,14 @@
 package com.example.wastenot.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,6 +16,10 @@ import com.example.wastenot.R;
 import com.squareup.picasso.Picasso;
 
 public class DescriptionActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -20,11 +29,26 @@ public class DescriptionActivity extends AppCompatActivity {
         int id = bundle.getInt("ID");
         String detailedDescription = bundle.getString("TITLE");
         String imageUrl = bundle.getString("IMAGE_URL");
+        Button navigate = (Button) findViewById(R.id.navigate);
         TextView idTxt = (TextView) findViewById(R.id.detailsText);
-        idTxt.setText("ID " + String.valueOf(id));
         TextView detailedDescTxt = (TextView) findViewById(R.id.detailedDescription);
-        detailedDescTxt.setText(detailedDescription);
         ImageView dishImage = (ImageView) findViewById(R.id.dishImage);
+        idTxt.setText("ID " + String.valueOf(id));
+        detailedDescTxt.setText(detailedDescription);
         Picasso.get().load(imageUrl).into(dishImage);
+
+        navigate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MapsActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
+
+
+
+
+
+
 }
