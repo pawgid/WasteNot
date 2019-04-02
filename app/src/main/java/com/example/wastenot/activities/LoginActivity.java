18286d297ec199185cde5e8b8b7de2cbc4384a6c
pawgid <pgidaszewski@gmail.com>
2019-04-02 22:10:46 +0200

    Added login activity

---
+++ app/src/main/java/com/example/wastenot/activities/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.wastenot.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.wastenot.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button loginButton = (Button) findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                v.getContext().startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/wastenot/activities/MapsActivity.java
+++ app/src/main/java/com/example/wastenot/activities/MapsActivity.java
@@ -61,12 +61,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
         locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById (R.id.fab);
-        fab.setOnClickListener (new View.OnClickListener () {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick (View view) {
-                Snackbar.make (view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction ("Action", null).show ();
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
